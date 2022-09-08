ba7d136444c54e449a809b4dd4f95252e41e4ef7
sas8254 <shashibhushansingh11@gmail.com>
2022-09-08 16:33:38 +0530

    adding Yelp Camp files to git

---
+++ seeds/index.js
@@ -0,0 +1,40 @@
+const Campground = require("../models/campground");
+const cities = require("./cities");
+const { descriptors, places } = require("./seedHelpers");
+const mongoose = require("mongoose");
+mongoose
+  .connect("mongodb://localhost:27017/YelpCamp", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("mongo Connection Open!!!");
+  })
+  .catch((err) => {
+    console.log("mongo error");
+    console.log(err);
+  });
+
+const sample = function (arrey) {
+  return arrey[Math.floor(Math.random() * arrey.length)];
+};
+
+const seedDB = async () => {
+  await Campground.deleteMany({});
+  for (let i = 0; i < 50; i++) {
+    const random1000 = Math.floor(Math.random() * 1000);
+    const price = Math.floor(Math.random() * 20 + 10);
+    const camp = new Campground({
+      location: `${cities[random1000].city}, ${cities[random1000].state}`,
+      title: `${sample(descriptors)} ${sample(places)}`,
+      image: "https://source.unsplash.com/collection/483251",
+      description:
+        "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Pariatur cum dolor asperiores! Iusto soluta nam quas sint quam exercitationem! A vel modi aut aperiam quidem eligendi quam placeat tempora illum?",
+      price,
+    });
+    await camp.save();
+  }
+};
+seedDB().then(() => {
+  mongoose.connection.close();
+});
