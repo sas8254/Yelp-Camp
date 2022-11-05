d4b1828231c2b987f576a6bc516bbf37a4f08715
sas8254 <shashibhushansingh11@gmail.com>
2022-11-05 10:50:55 +0530

    492 configring session

--- app.js
+++ app.js
@@ -3,11 +3,10 @@ const path = require("path");
 const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
 const ExpressError = require("./utils/ExpressError");
-const mongoose = require("mongoose");
-
+const session = require("express-session");
 const campgrounds = require("./routes/campground");
 const reviews = require("./routes/reviews");
-
+const mongoose = require("mongoose");
 mongoose
   .connect("mongodb://localhost:27017/YelpCamp", {
     useNewUrlParser: true,
@@ -29,6 +28,19 @@ app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
 app.use(express.static(path.join(__dirname, "public")));
+
+const sessionConfig = {
+  secret: "donttelltoanyonse",
+  resave: false,
+  saveUninitialized: true,
+  cookie: {
+    httpOnly: true,
+    expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
+    maxAge: 1000 * 60 * 60 * 24 * 7,
+  },
+};
+
+app.use(session(sessionConfig));
 
 app.use("/campgrounds", campgrounds);
 app.use("/campgrounds/:id/reviews", reviews);
