a29302072fa936adba567a8c6ebea8f426581970
sas8254 <shashibhushansingh11@gmail.com>
2022-11-08 16:45:57 +0530

    in middle

--- app.js
+++ app.js
@@ -7,6 +7,10 @@ const session = require("express-session");
 const flash = require("connect-flash");
 const campgrounds = require("./routes/campground");
 const reviews = require("./routes/reviews");
+const passport = require("passport");
+const LocalStrategy = require("passport-local");
+const User = require("./models/user");
+
 const mongoose = require("mongoose");
 mongoose
   .connect("mongodb://localhost:27017/YelpCamp", {
@@ -43,6 +47,10 @@ const sessionConfig = {
 
 app.use(session(sessionConfig));
 app.use(flash());
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+
 app.use((req, res, next) => {
   res.locals.success = req.flash("success");
   res.locals.error = req.flash("error");
--- models/user.js
+++ models/user.js
@@ -10,6 +10,6 @@ const UserSchema = new Schema({
   },
 });
 
-userSchema.plugin(passportLocalMongoose);
+UserSchema.plugin(passportLocalMongoose);
 
 module.exports = mongoose.model("User", UserSchema);
