6987bba5e291a15b1e1fdfb1e24e5288547a954c
shashibhushan <shashibhushansingh11@gmail.com>
2022-11-13 19:13:08 +0530

    528. A Fancy Way To Restructure Routes.

--- routes/campground.js
+++ routes/campground.js
@@ -4,39 +4,33 @@ const campgrounds = require("../controllers/campgrounds");
 const catchAsync = require("../utils/catchAsync");
 const { isLoggedIn, isAuther, validateCampground } = require("../middleware");
 
-router.get("/", catchAsync(campgrounds.index));
+router
+  .route("/")
+  .get(catchAsync(campgrounds.index))
+  .post(
+    isLoggedIn,
+    validateCampground,
+    catchAsync(campgrounds.createCampground)
+  );
 
 router.get("/new", isLoggedIn, campgrounds.renderNewForm);
 
-router.post(
-  "/",
-  isLoggedIn,
-  validateCampground,
-  catchAsync(campgrounds.createCampground)
-);
-
-router.get("/:id", catchAsync(campgrounds.showCampground));
+router
+  .route("/:id")
+  .get(catchAsync(campgrounds.showCampground))
+  .put(
+    isLoggedIn,
+    isAuther,
+    validateCampground,
+    catchAsync(campgrounds.updateCampground)
+  )
+  .delete(isLoggedIn, isAuther, catchAsync(campgrounds.deleteCampground));
 
 router.get(
   "/:id/edit",
   isLoggedIn,
   isAuther,
   catchAsync(campgrounds.renderEditForm)
-);
-
-router.put(
-  "/:id",
-  isLoggedIn,
-  isAuther,
-  validateCampground,
-  catchAsync(campgrounds.updateCampground)
-);
-
-router.delete(
-  "/:id",
-  isLoggedIn,
-  isAuther,
-  catchAsync(campgrounds.deleteCampground)
 );
 
 module.exports = router;
