1bad570cee555ff819f86180ce3ff6686b59d420
sas8254 <shashibhushansingh11@gmail.com>
2022-11-25 15:51:29 +0530

    566. Mongo Injection.

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ const flash = require("connect-flash");
 const passport = require("passport");
 const LocalStrategy = require("passport-local");
 const User = require("./models/user");
+const mongoSanitize = require("express-mongo-sanitize");
 
 const campgroundRoutes = require("./routes/campground");
 const reviewRoutes = require("./routes/reviews");
@@ -39,6 +40,11 @@ app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
 app.use(express.static(path.join(__dirname, "public")));
+app.use(
+  mongoSanitize({
+    replaceWith: "_",
+  })
+);
 
 const sessionConfig = {
   secret: "donttelltoanyonse",
@@ -61,6 +67,7 @@ passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
 app.use((req, res, next) => {
+  console.log(req.query);
   res.locals.currentUser = req.user;
   res.locals.success = req.flash("success");
   res.locals.error = req.flash("error");
