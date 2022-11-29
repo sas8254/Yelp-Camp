79cf55b768a9c5ff7d532de631ffe7cb6573f513
sas8254 <shashibhushansingh11@gmail.com>
2022-11-29 11:53:39 +0530

    change map styles to streets

--- public/javascripts/showPageMap.js
+++ public/javascripts/showPageMap.js
@@ -1,7 +1,7 @@
 mapboxgl.accessToken = mapToken;
 const map = new mapboxgl.Map({
   container: "map",
-  style: "mapbox://styles/mapbox/light-v10",
+  style: "mapbox://styles/mapbox/streets-v11",
   center: campground.geometry.coordinates,
   zoom: 11,
 });
